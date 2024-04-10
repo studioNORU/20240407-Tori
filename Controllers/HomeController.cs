db9b1a3bf3c92c7400f7599e9b3563afbc0203d4
gze1206 <gze1206@naver.com>
2024-04-10 19:01:01 +0900

    feat: Add Swagger

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,9 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Tori.Controllers;
 
+[ApiController]
+[Route("[controller]/[action]")]
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> logger;
@@ -12,18 +13,12 @@ public class HomeController : Controller
         this.logger = logger;
     }
 
+    [HttpGet("")]
     public IActionResult Index()
     {
         return this.Json(new
         {
             test = "Test",
         });
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        this.logger.Log(LogLevel.Error, "REQ : {reqId}", Activity.Current?.Id ?? this.HttpContext.TraceIdentifier);
-        return this.Problem();
     }
 }
