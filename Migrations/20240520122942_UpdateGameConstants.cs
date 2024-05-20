bed20bb78dcf19b7217403ce3ac936c5a0b8c856
gze1206 <gze1206@naver.com>
2024-05-20 21:37:48 +0900

    feat: Update game constants

---
+++ Migrations/20240520122942_UpdateGameConstants.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace tori.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateGameConstants : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "GameConstants",
+                keyColumn: "Key",
+                keyValue: "CONST_SYSTEM_STEAL_ITEM_RANGE");
+
+            migrationBuilder.InsertData(
+                table: "GameConstants",
+                columns: new[] { "Key", "Value" },
+                values: new object[,]
+                {
+                    { "CONST_SYSTEM_SMOKE_DURATION", 1500 },
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "GameConstants",
+                keyColumn: "Key",
+                keyValue: "CONST_SYSTEM_SMOKE_DURATION");
+            
+            migrationBuilder.InsertData(
+                table: "GameConstants",
+                columns: new[] { "Key", "Value" },
+                values: new object[,]
+                {
+                    { "CONST_SYSTEM_STEAL_ITEM_RANGE", 5000 },
+                });
+        }
+    }
+}
