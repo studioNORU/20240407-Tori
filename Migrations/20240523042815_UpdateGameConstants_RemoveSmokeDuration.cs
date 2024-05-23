5fdbc054a770a91f3bde2850030a9136f24c17e3
gze1206 <gze1206@naver.com>
2024-05-23 14:32:02 +0900

    feat: Apply updated game constants

---
+++ Migrations/20240523042815_UpdateGameConstants_RemoveSmokeDuration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace tori.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateGameConstants_RemoveSmokeDuration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "GameConstants",
+                keyColumn: "Key",
+                keyValue: "CONST_SYSTEM_SMOKE_DURATION");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "GameConstants",
+                columns: new[] { "Key", "Value" },
+                values: new object[,]
+                {
+                    { "CONST_SYSTEM_SMOKE_DURATION", 1500 },
+                });
+        }
+    }
+}
